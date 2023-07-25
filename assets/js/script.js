13e44af2a5a8ff71805cff5d8566e9ed8cdc21ad
JustinFourie1993 <>
2023-07-25 15:19:57 +0000

    add matched card function and shuffledeck function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,61 @@
 let cards = document.getElementsByClassName('card');
 
-for (card in cards) {
-    addEventListener("click", turnCard);
-}
-
+let matchedCards = 0;
 let cardOne, cardTwo;
+let disableDeck = false;
 
 function turnCard(e) {
     let clickedCard = e.target;
-    if (clickedCard !== cardOne) {
-        clickedCard.classList.add("turn");
+    if (clickedCard !== cardOne && !disableDeck) {
+        clickedCard.classList.add('turn');
         if (!cardOne) {
             return cardOne = clickedCard;
         }
         cardTwo = clickedCard;
-        console.log(cardOne, cardTwo);
+        disableDeck = true;
+
+        let cardOneImg = cardOne.querySelector('.card-front img').src;
+        let cardTwoImg = cardTwo.querySelector('.card-front img').src;
+        matchCards(cardOneImg, cardTwoImg);
     }
 }
 
+function matchCards(img1, img2) {
+    if (img1 === img2) {
+        matchedCards++;
+        if (matchedCards == 8) {
+            shuffleDeck();
+        }
+        cardOne.removeEventListener("click", turnCard);
+        cardTwo.removeEventListener("click", turnCard);
+        cardOne = cardTwo = "";
+        return disableDeck = false;
+    }
+
+    setTimeout(() => {
+        cardOne.classList.remove('turn');
+        cardTwo.classList.remove('turn');
+        cardOne = cardTwo = "";
+        disableDeck = false;
+    }, 1000);
+
+}
+
+function shuffleDeck() {
+    matchedCards = 0;
+    cardOne = cardTwo = "";
+    for (card in cards) {
+        document.getElementsByClassName('turn').classList.remove('turn');
+        addEventListener('click', turnCard);
+    }
+
+
+}
+
+
+for (card in cards) {
+    addEventListener('click', turnCard);
+}
 
 
 
