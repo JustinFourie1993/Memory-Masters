7f4fc4ae244af3a4b3c9b293fdd0241fdf3e96ba
JustinFourie1993 <>
2023-07-26 14:13:43 +0000

    Tidy up code and add comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,14 @@
-let cards = document.querySelectorAll(".card");
+let cards = document.querySelectorAll(".card");   //getting all cards wich class "card".
 
 let matchedCards = 0;
 let cardOne, cardTwo;
 let disableDeck = false;
 
+cards.forEach(card => {  //Loop to add event listeners to cards, when clicked, call funtion turnCard() to flip card.
+    card.addEventListener("click", turnCard);
+});
+
+//function to turn over card that is clicked by adding "turn" class.
 function turnCard(e) {
     let clickedCard = e.target;
     if (clickedCard !== cardOne && !disableDeck) {
@@ -14,27 +19,29 @@ function turnCard(e) {
         cardTwo = clickedCard;
         disableDeck = true;
 
-        let cardOneImg = cardOne.querySelector(".card-front img").src;
+        let cardOneImg = cardOne.querySelector(".card-front img").src;  //Assigning images.
         let cardTwoImg = cardTwo.querySelector(".card-front img").src;
-        matchCards(cardOneImg, cardTwoImg);
+        matchCards(cardOneImg, cardTwoImg);  //Call function to check if cards matched.
     }
 }
 
+//Function to check if cards match.
 function matchCards(img1, img2) {
     if (img1 === img2) {
         matchedCards++;
-        if (matchedCards == 8) {
+        if (matchedCards == 8) {   //Once all cards are matched, the deck is shuffled after 2 sec.
             setTimeout(() => {
                 return shuffleDeck();
-            }, 3000);
+            }, 2000);
 
         }
-        cardOne.removeEventListener("click", turnCard);
+        cardOne.removeEventListener("click", turnCard); //Removes click to turn so matched cards stay face up.
         cardTwo.removeEventListener("click", turnCard);
-        cardOne = cardTwo = "";
+        cardOne = cardTwo = "";    //Sets value to blank.
         return disableDeck = false;
     }
 
+    //Funtion to remove turn class if cards not matched (they flip back).
     setTimeout(() => {
         cardOne.classList.remove("turn");
         cardTwo.classList.remove("turn");
@@ -44,31 +51,28 @@ function matchCards(img1, img2) {
 
 }
 
+//Function to randomize card placement and flip cards back over
 function shuffleDeck() {
     disableDeck = false;
     matchedCards = 0;
     cardOne = cardTwo = "";
+    //Array with each available card name.
     let myArray = ['ace-spades', 'king-spades', 'queen-spades', 'jack-spades', 'ace-hearts', 'king-hearts', 'queen-hearts', 'jack-hearts',
         'ace-spades', 'king-spades', 'queen-spades', 'jack-spades', 'ace-hearts', 'king-hearts', 'queen-hearts', 'jack-hearts'];
-    myArray.sort(() => Math.random() - 0.5);
+    myArray.sort(() => Math.random() - 0.5);  //Sorts array randomly.
 
-
-
+    //Loop to alter every card
     cards.forEach((card, index) => {
-        card.classList.remove("turn");
+        card.classList.remove("turn");  //Remove "turn" class, all cards now face down
         let imgTag = card.querySelector(".pic");
-        imgTag.src = `assets/images/${myArray[index]}.png`;
-        card.addEventListener("click", turnCard);
+        imgTag.src = `assets/images/${myArray[index]}.png`; //Assigns each cards img from the randomly sorted array.
+        card.addEventListener("click", turnCard);   //Reasigns event listener to turn cards if clicked.
     });
-
-
 }
 
-shuffleDeck();
+shuffleDeck();  //Deck is shuffled when page loads
 
-cards.forEach(card => {
-    card.addEventListener("click", turnCard);
-});
+
 
 
 
