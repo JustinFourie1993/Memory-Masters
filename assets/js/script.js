ee7e3ae03abce29d9f0e49223e49a6979699e5aa
JustinFourie1993 <>
2023-10-23 12:06:07 +0000

    add function to check if won, reset game, stop timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
     const introModal = document.getElementById("intro-modal");
     const startGameButton = document.getElementById("start-game-button");
+    const winModal = document.getElementById("win-modal");
+    winModal.style.display = "none";
 
-
-    // Close the introduction modal and start the game when the "Start Game" button is clicked
+    // Close the introduction modal and start the game when the "Start Game" 
+    // button is clicked
     startGameButton.addEventListener("click", function () {
         introModal.style.display = "none";
         startGame();
@@ -11,8 +13,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
 });
 
+// function to set game in play
 function startGame() {
-
     let cards = document.querySelectorAll(".card");
     let matchedCards = 0;
     let cardOne, cardTwo;
@@ -20,7 +22,7 @@ function startGame() {
     let moves = 0;
     let timerInterval;
     let seconds = 0;
-    let isGameWon = false;
+
 
     const movesDisplay = document.querySelector(".moves");
     const timerDisplay = document.querySelector(".timer");
@@ -29,11 +31,13 @@ function startGame() {
     const finalTime = document.getElementById("final-time");
     const finalMoves = document.getElementById("final-moves");
 
+    // displays ammount of moves
     function incrementMoves() {
         moves++;
         movesDisplay.textContent = `Moves: ${moves}`;
     }
 
+    // displays time played
     function startTimer() {
         timerInterval = setInterval(() => {
             seconds++;
@@ -47,19 +51,41 @@ function startGame() {
         clearInterval(timerInterval);
     }
 
+    // stops timer and displays win modal if all cards are matched
+    function checkWin() {
+        if (matchedCards === 8) {
+            stopTimer();
+            setTimeout(() => {
+                showWinModal();
+            }, 1000);
+        }
+    }
+
+    // displays win modal with final time and moves made
     function showWinModal() {
         finalTime.textContent = `${seconds}s`;
         finalMoves.textContent = moves;
+        winModal.style.display = "block";
     }
 
-    //Loop to add event listeners to cards, when clicked, call funtion turnCard() 
-    cards.forEach(card => {
+    // resets timer, moves, shuffles the deck and hides win modal
+    function resetGame() {
+        stopTimer();
+        matchedCards = 0;
+        moves = 0;
+        seconds = 0;
+        movesDisplay.textContent = "Moves: 0";
+        timerDisplay.textContent = "Time: 0s";
+        shuffleDeck();
+        winModal.style.display = "none";
+        startTimer();
+    }
 
-        card.addEventListener("click", turnCard);
-    });
+    resetButton.addEventListener("click", resetGame);
 
-    //function to turn over card that is clicked by adding "turn" class.
-    // code taken from https://www.youtube.com/watch?v=DABkhfsBAWw&t=1553s
+    // turncard function taken from tuturial 
+    // https://www.youtube.com/watch?v=DABkhfsBAWw&t=1553s
+    // adds a class to clicked cards to turn them over, and check if matched
     function turnCard(e) {
         let clickedCard = e.target;
         if (clickedCard !== cardOne && !disableDeck) {
@@ -71,60 +97,55 @@ function startGame() {
             cardTwo = clickedCard;
             disableDeck = true;
 
-            let cardOneImg = cardOne.querySelector(".card-front img").src;  //Assigning images.
+            let cardOneImg = cardOne.querySelector(".card-front img").src;
             let cardTwoImg = cardTwo.querySelector(".card-front img").src;
-            matchCards(cardOneImg, cardTwoImg);  //Call function to check if cards matched.
+            matchCards(cardOneImg, cardTwoImg);
         }
     }
 
-    //Function to check if cards match.
-    // code taken from https://www.youtube.com/watch?v=DABkhfsBAWw&t=1553s
+    // matchCards function taken from tuturial 
+    // https://www.youtube.com/watch?v=DABkhfsBAWw&t=1553s
+    // if cards match, remove click event, else remove class that 
+    // has flipped them over.  And check if game is won
     function matchCards(img1, img2) {
         if (img1 === img2) {
             matchedCards++;
-            if (matchedCards == 8) {  
-                stopTimer()
-            }
-            cardOne.removeEventListener("click", turnCard); //Removes click to turn so matched cards stay face up.
+            cardOne.removeEventListener("click", turnCard);
             cardTwo.removeEventListener("click", turnCard);
-            cardOne = cardTwo = "";    //Sets value to blank.
-            return disableDeck = false;
-        }else{}
-
-        //Funtion to remove turn class if cards not matched (they flip back).
-        // code taken from https://www.youtube.com/watch?v=DABkhfsBAWw&t=1553s
-        setTimeout(() => {
-            cardOne.classList.remove("turn");
-            cardTwo.classList.remove("turn");
             cardOne = cardTwo = "";
             disableDeck = false;
-        }, 1000);}
-
+            checkWin();
+        } else {
+            setTimeout(() => {
+                cardOne.classList.remove("turn");
+                cardTwo.classList.remove("turn");
+                cardOne = cardTwo = "";
+                disableDeck = false;
+            }, 1000);
+        }
     }
 
-    //Function to randomize card placement and flip cards back over
-    // code taken from https://www.youtube.com/watch?v=DABkhfsBAWw&t=1553s
+    // shuffleDeck function taken from tuturial 
+    // https://www.youtube.com/watch?v=DABkhfsBAWw&t=1553s
+    // resets deck, creates an array of all cards and randomizes it, then 
+    // assigns an image to each card, removes turn class, reasigns click event
     function shuffleDeck() {
         disableDeck = false;
         matchedCards = 0;
         cardOne = cardTwo = "";
-        //Array with each available card name.
         let myArray = ['ace-spades', 'king-spades', 'queen-spades', 'jack-spades', 'ace-hearts', 'king-hearts', 'queen-hearts', 'jack-hearts',
             'ace-spades', 'king-spades', 'queen-spades', 'jack-spades', 'ace-hearts', 'king-hearts', 'queen-hearts', 'jack-hearts'];
-        myArray.sort(() => Math.random() - 0.5);  //Sorts array randomly.
+        myArray.sort(() => Math.random() - 0.5);
 
-        //Loop to alter every card
         cards.forEach((card, index) => {
-            card.classList.remove("turn");  //Remove "turn" class, all cards now face down
+            card.classList.remove("turn");
             let imgTag = card.querySelector(".pic");
-            imgTag.src = `assets/images/${myArray[index]}.png`; //Assigns each cards img from the randomly sorted array.
-            card.addEventListener("click", turnCard);   //Reasigns event listener to turn cards if clicked.
+            imgTag.src = `assets/images/${myArray[index]}.png`;
+            card.addEventListener("click", turnCard);
         });
+    }
 
-     
-        
-    };
+    shuffleDeck();
+}
 
-    shuffleDeck();  //Deck is shuffled when page loads
 
-};
